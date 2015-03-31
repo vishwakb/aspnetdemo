ae4315d82f0b000a2924d0a7625d316108bb23dd
vkarmendra <vkbhardwaj21@gmail.com>
2015-03-31 13:04:21 +0530

    first commit

---
+++ AspNetDemo/AspNetDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace AspNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = "http://+:8080";
+
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine("Server Started");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
