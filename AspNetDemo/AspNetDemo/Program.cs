fcb4890f778c5792563c9fb9025fee75f267fc66
vkarmendra <vkbhardwaj21@gmail.com>
2015-03-31 18:26:45 +0530

    Program file changes

--- AspNetDemo/AspNetDemo/Program.cs
+++ AspNetDemo/AspNetDemo/Program.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Owin.Hosting;
 using Owin;
 
 namespace AspNetDemo
 {
-    class Program
+    public class Program
     {
+        private static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
-            var baseAddress = "http://+:8080";
-
-            using (WebApp.Start<Startup>(baseAddress))
+            var port = 5000;
+            if (args.Length > 0)
             {
-                Console.WriteLine("Server Started");
-                Console.ReadLine();
+                int.TryParse(args[0], out port);
+            }
 
+            Console.CancelKeyPress += (sender, eArgs) =>
+            {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
+
+            using (WebApp.Start<Startup>(string.Format("http://*:{0}", port)))
+            {
+                Console.WriteLine("Started");
+                _quitEvent.WaitOne();
             }
         }
     }
