29f7b9bae6793d373a2708e2070031fbcc51bd51
unknown <vishwa.karmendra@igt.in>
2015-08-17 11:04:53 +0530

    Resolved commit.

--- AspNetDemo/AspNetDemo/DemoClass.cs
+++ AspNetDemo/AspNetDemo/DemoClass.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace AspNetDemo
 {
-    class DemoClass
+    public class DemoClass
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
